448f61b0d7cef80784e0f7f2cf9efc68750da69f
ThomasAlmunaPrieto <thomas.almuna@alumnos.ucn.cl>
2024-05-26 21:06:40 -0400

    pequeño commit de actualizacion

---
+++ cmake-build-debug/CMakeFiles/Taller2_ED.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/AVL.cpp" "CMakeFiles/Taller2_ED.dir/AVL.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/AVL.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/MinHeap.cpp" "CMakeFiles/Taller2_ED.dir/MinHeap.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/MinHeap.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/enemigo.cpp" "CMakeFiles/Taller2_ED.dir/enemigo.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/enemigo.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/habilidades.cpp" "CMakeFiles/Taller2_ED.dir/habilidades.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/habilidades.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/habitacion.cpp" "CMakeFiles/Taller2_ED.dir/habitacion.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/habitacion.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/jugador.cpp" "CMakeFiles/Taller2_ED.dir/jugador.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/jugador.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/main.cpp" "CMakeFiles/Taller2_ED.dir/main.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/main.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/menus.cpp" "CMakeFiles/Taller2_ED.dir/menus.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/menus.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/nodoHabilidad.cpp" "CMakeFiles/Taller2_ED.dir/nodoHabilidad.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/nodoHabilidad.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/nodoHabitacion.cpp" "CMakeFiles/Taller2_ED.dir/nodoHabitacion.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/nodoHabitacion.cpp.obj.d"
+  "C:/Users/Thomas/Documents/GitHub/Taller2_ED/objetos.cpp" "CMakeFiles/Taller2_ED.dir/objetos.cpp.obj" "gcc" "CMakeFiles/Taller2_ED.dir/objetos.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Taller2_ED.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Taller2_ED.dir/AVL.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/AVL.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/MinHeap.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/MinHeap.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/enemigo.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/enemigo.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/habilidades.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/habilidades.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/habitacion.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/habitacion.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/jugador.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/jugador.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/main.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/main.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/menus.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/menus.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/nodoHabilidad.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/nodoHabilidad.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/nodoHabitacion.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/nodoHabitacion.cpp.obj.d"
+  "CMakeFiles/Taller2_ED.dir/objetos.cpp.obj"
+  "CMakeFiles/Taller2_ED.dir/objetos.cpp.obj.d"
+  "Taller2_ED.exe"
+  "Taller2_ED.exe.manifest"
+  "Taller2_ED.pdb"
+  "libTaller2_ED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Taller2_ED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
